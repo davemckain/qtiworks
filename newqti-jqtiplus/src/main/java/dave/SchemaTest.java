3f80b7dc3a188a063f1799ac032dc8342e9a1746
David McKain <david.mckain@ed.ac.uk>
2012-01-17 21:58:24 +0000

    Updated to use new API

--- newqti-jqtiplus/src/main/java/dave/SchemaTest.java
+++ newqti-jqtiplus/src/main/java/dave/SchemaTest.java
@@ -6,37 +6,27 @@
 package dave;
 
 import uk.ac.ed.ph.jqtiplus.JqtiExtensionManager;
-import uk.ac.ed.ph.jqtiplus.node.item.AssessmentItem;
+import uk.ac.ed.ph.jqtiplus.internal.util.DumpMode;
+import uk.ac.ed.ph.jqtiplus.internal.util.ObjectDumper;
+import uk.ac.ed.ph.jqtiplus.reading.QtiXmlObjectReader;
+import uk.ac.ed.ph.jqtiplus.resolution.AssessmentObjectManager;
+import uk.ac.ed.ph.jqtiplus.validation.ItemValidationResult;
 import uk.ac.ed.ph.jqtiplus.xmlutils.ClassPathResourceLocator;
-import uk.ac.ed.ph.jqtiplus.xmlutils.legacy.AssessmentItemManager;
-import uk.ac.ed.ph.jqtiplus.xmlutils.legacy.QTIObjectManager;
-import uk.ac.ed.ph.jqtiplus.xmlutils.legacy.QTIReadResult;
-import uk.ac.ed.ph.jqtiplus.xmlutils.legacy.SimpleQTIObjectCache;
-import uk.ac.ed.ph.jqtiplus.xmlutils.legacy.SupportedXMLReader;
-import uk.ac.ed.ph.jqtiplus.xmlutils.legacy.XMLParseResult;
 
 import java.net.URI;
 
 public class SchemaTest {
     
     public static void main(String[] args) throws Exception {
+        URI inputUri = URI.create("classpath:/mathextensions.xml");
+        
+        System.out.println("Reading and validating");
         JqtiExtensionManager jqtiExtensionManager = new JqtiExtensionManager();
-        SupportedXMLReader xmlReader = new SupportedXMLReader(true);
-        QTIObjectManager objectManager = new QTIObjectManager(jqtiExtensionManager, xmlReader, new ClassPathResourceLocator(), new SimpleQTIObjectCache());
+        QtiXmlObjectReader objectReader = new QtiXmlObjectReader(jqtiExtensionManager, new ClassPathResourceLocator());
         
-        URI inputFileUri = URI.create("classpath:/mathextensions.xml");
-        QTIReadResult<AssessmentItem> result = objectManager.getQTIObject(inputFileUri, AssessmentItem.class);
-        AssessmentItem item = result.getJQTIObject();
-        
-        XMLParseResult xmlParseResult = result.getXMLParseResult();
-        System.out.println("Fatal errors are: " + xmlParseResult.getFatalErrors());
-        System.out.println("Errors are: " + xmlParseResult.getErrors());
-        System.out.println("Warnings are: " + xmlParseResult.getWarnings());
-        System.out.println("Supported schemas: " + xmlParseResult.getSupportedSchemaNamespaces());
-        
-        System.out.println("Result: " + result);
-        
-        AssessmentItemManager itemManager = new AssessmentItemManager(objectManager, item);
-        System.out.println("JQTI validation: " + itemManager.validateItem());
+        AssessmentObjectManager objectManager = new AssessmentObjectManager(objectReader);
+
+        ItemValidationResult result = objectManager.validateItem(inputUri);
+        System.out.println("Validation result: " + ObjectDumper.dumpObject(result, DumpMode.DEEP));
     }
 }
