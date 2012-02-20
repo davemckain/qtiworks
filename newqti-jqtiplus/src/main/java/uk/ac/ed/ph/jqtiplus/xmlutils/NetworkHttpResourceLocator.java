7cb160657ade925d375959a0783c305d5af5b38d
David McKain <david.mckain@ed.ac.uk>
2012-02-20 14:17:26 +0000

    This should have been added a few commits back

---
+++ newqti-jqtiplus/src/main/java/uk/ac/ed/ph/jqtiplus/xmlutils/NetworkHttpResourceLocator.java
@@ -0,0 +1,79 @@
+/* Copyright (c) 2012, University of Edinburgh.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * * Neither the name of the University of Edinburgh nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * This software is derived from (and contains code from) QTItools and MathAssessEngine.
+ * QTItools is (c) 2008, University of Southampton.
+ * MathAssessEngine is (c) 2010, University of Edinburgh.
+ */
+package uk.ac.ed.ph.jqtiplus.xmlutils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Implementation of {@link ResourceLocator} that loads HTTP
+ * resources directly over the network.
+ * 
+ * @author David McKain
+ */
+public final class NetworkHttpResourceLocator implements ResourceLocator {
+
+    private static final Logger logger = LoggerFactory.getLogger(NetworkHttpResourceLocator.class);
+
+    @Override
+    public InputStream findResource(final URI systemId) {
+        final String scheme = systemId.getScheme();
+        if ("http".equals(scheme)) {
+            return loadHttpResource(systemId);
+        }
+        return null;
+    }
+
+    private InputStream loadHttpResource(final URI systemId) {
+        InputStream resourceStream;
+        try {
+            resourceStream = systemId.toURL().openConnection().getInputStream();
+            logger.debug("Successful connection to HTTP resource with URI {}", systemId);
+        }
+        catch (IOException e) {
+            resourceStream = null;
+            logger.warn("Failed to open connection to HTTP resource with URI {}", systemId);
+        }
+        return resourceStream;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "@" + hashCode();
+    }
+}
