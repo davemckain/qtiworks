ecd9598d148effdfe08756e6b6eed4aba2479c85
David McKain <david.mckain@ed.ac.uk>
2012-03-22 09:34:34 +0000

    Synced engine + added ASCIIMath verifier service

---
+++ qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/web/controller/CandidateHelperController.java
@@ -0,0 +1,75 @@
+/* Copyright (c) 2012, University of Edinburgh.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * * Neither the name of the University of Edinburgh nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * This software is derived from (and contains code from) QTItools and MathAssessEngine.
+ * QTItools is (c) 2008, University of Southampton.
+ * MathAssessEngine is (c) 2010, University of Edinburgh.
+ */
+package uk.ac.ed.ph.qtiworks.web.controller;
+
+import uk.ac.ed.ph.snuggletex.utilities.StylesheetCache;
+
+import org.qtitools.mathassess.tools.qticasbridge.ASCIIMathMLHelper;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Controller for candidate helpers, such as math input verifiers.
+ *
+ * @author David McKain
+ */
+@Controller
+public class CandidateHelperController {
+    
+    @Resource
+    private StylesheetCache stylesheetCache;
+
+    /**
+     * Runs {@link ASCIIMathMLHelper} helper on the given 'input' parameter, expecting to return
+     * JSON.
+     * 
+     * Accept: application/json from client expected
+     */
+    @RequestMapping(value="/verifyAsciiMath", method=RequestMethod.POST)
+    @ResponseBody
+    public Map<String, String>  verifyASCIIMath(@RequestParam("input") String asciiMathInput) {
+        ASCIIMathMLHelper asciiMathHelper = new ASCIIMathMLHelper(stylesheetCache);
+        Map<String, String> upConvertedASCIIMathInput = asciiMathHelper.upConvertASCIIMathInput(asciiMathInput);
+        return upConvertedASCIIMathInput;
+    }
+
+}
