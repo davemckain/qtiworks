1b4721b32e351f6c003c369e8ff5f395a050f750
David McKain <david.mckain@ed.ac.uk>
2012-03-28 14:11:16 +0100

    Now coalesces adjacent runs of text

--- qtiworks-jqtiplus/src/main/java/uk/ac/ed/ph/jqtiplus/xmlutils/SimpleDomBuilderHandler.java
+++ qtiworks-jqtiplus/src/main/java/uk/ac/ed/ph/jqtiplus/xmlutils/SimpleDomBuilderHandler.java
@@ -44,21 +44,32 @@ import org.xml.sax.helpers.DefaultHandler;
 /**
  * Trivial SAX {@link DefaultHandler} that builds a DOM {@link Document}, handling only the types of
  * SAX events we'd expect in documents that do not contain complex DTDs.
+ * <p>
+ * This coalesces adjacent runs of characters into single text Nodes.
+ * <p>
+ * Usage: not thread safe, not reusable.
  * 
  * @author David McKain
  */
 public final class SimpleDomBuilderHandler extends DefaultHandler {
 
+    /** Document being build */
     private final Document document;
 
+    /** SAX Locator details */
     private Locator locator;
 
+    /** Current DOM Node being built */
     private Node currentNode;
+    
+    /** Coalesces SAX character events */
+    private final StringBuilder textNodeBuilder;
 
     public SimpleDomBuilderHandler(Document document) {
         this.document = document;
         this.locator = null;
         this.currentNode = null;
+        this.textNodeBuilder = new StringBuilder();
     }
 
     @Override
@@ -73,6 +84,7 @@ public final class SimpleDomBuilderHandler extends DefaultHandler {
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) {
+        addAnyCoalescedText();
         final Element newElement = document.createElementNS(uri, qName);
         for (int i = 0, length = attributes.getLength(); i < length; i++) {
             newElement.setAttributeNS(attributes.getURI(i), attributes.getQName(i), attributes.getValue(i));
@@ -84,10 +96,11 @@ public final class SimpleDomBuilderHandler extends DefaultHandler {
 
     @Override
     public void characters(char[] ch, int start, int length) {
-        final String text = new String(ch, start, length);
-        final Text textNode = document.createTextNode(text);
-        storeLocationInformation(textNode);
-        currentNode.appendChild(textNode);
+        /* (Coalesce adjacent runs of characters together. This often happens when using NCRs or entities) */
+        if (currentNode == null) {
+            throw new IllegalStateException("Inconsistent state at characters: currentNode==null");
+        }
+        textNodeBuilder.append(ch, start, length);
     }
 
     @Override
@@ -95,13 +108,26 @@ public final class SimpleDomBuilderHandler extends DefaultHandler {
         if (currentNode == null) {
             throw new IllegalStateException("Inconsistent state at endElement: currentNode==null");
         }
+        addAnyCoalescedText();
         currentNode = currentNode.getParentNode();
+    }
+    
+    private void addAnyCoalescedText() {
+        if (textNodeBuilder.length()>0) {
+            final Text textNode = document.createTextNode(textNodeBuilder.toString());
+            storeLocationInformation(textNode);
+            currentNode.appendChild(textNode);
+            textNodeBuilder.setLength(0);
+        }
     }
 
     @Override
     public void endDocument() {
         if (currentNode != document) {
             throw new IllegalStateException("Inconsistent state at endDocument: currentNode=" + currentNode);
+        }
+        if (textNodeBuilder.length() > 0) {
+            throw new IllegalStateException("Inconsistent state at endDocument: textNodeBuilder contains " + textNodeBuilder.toString());            
         }
     }
 
