74a6dca412b7e221dddc0d867e2124c9f8a3a424
David McKain <david.mckain@ed.ac.uk>
2012-05-22 09:06:05 +0100

    Fixed merge on this class in last commit. Doh.

--- qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/config/PublicMvcConfiguration.java
+++ qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/config/PublicMvcConfiguration.java
@@ -33,16 +33,17 @@
  */
 package uk.ac.ed.ph.qtiworks.config;
 
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-<<<<<<< HEAD
-=======
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
->>>>>>> origin/master
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
