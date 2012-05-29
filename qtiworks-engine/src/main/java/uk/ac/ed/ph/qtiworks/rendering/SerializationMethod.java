812bccc6bbd97cbd712f042447b99ea733343797
David McKain <david.mckain@ed.ac.uk>
2012-05-29 14:18:34 +0100

    Removed old AssessmentRendererV1

--- qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/rendering/SerializationMethod.java
+++ qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/rendering/SerializationMethod.java
@@ -35,45 +35,45 @@ package uk.ac.ed.ph.qtiworks.rendering;
 
 /**
  * Encapsulates the supported ways of serializing the results obtained
- * from {@link AssessmentRendererV1}.
- * 
+ * from {@link AssessmentRenderer}.
+ *
  * @author  David McKain
  */
 public enum SerializationMethod {
-    
-    /** 
+
+    /**
      * XHTML + MathML, mis-delivered as text/html, using MathJax to display
      * any MathML that is present.
      */
     XHTML_MATHJAX("text/html", "xhtml"),
-    
-    /** 
+
+    /**
      * HTML5 + MathML, using MathJax to display any MathML that is present.
      */
     HTML5_MATHJAX("text/html", "html"),
-    
-    /** 
+
+    /**
      * XHTML + MathML delivered as application/xhtml+xml.
      * Perfect for MathML items on Mozilla.
      */
     MOZILLA_MATHML("application/xhtml+xml", "xhtml"),
-    
+
     /**
      * XHTML mis-delivered as text/html containing gubbins to invoke MathPlayer
      */
     IE_MATHPLAYER("text/html", "html"),
-    
-    /** 
+
+    /**
      * XHTML traditionally mis-delivered as text/html.
      * Perfect for non-MathML items, useless otherwise.
      */
     TRADITIONAL_XHTML("text/html", "xhtml"),
-    
+
     ;
-    
+
     private final String contentType;
     private final String method;
-    
+
     private SerializationMethod(final String contentType, final String method) {
         this.contentType = contentType;
         this.method = method;
