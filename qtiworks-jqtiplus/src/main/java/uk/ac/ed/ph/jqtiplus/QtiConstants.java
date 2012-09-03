6d610762e8907280def7db60211968fb67673395
David McKain <david.mckain@ed.ac.uk>
2012-09-03 09:59:25 +0100

    Merged in latest (final?) XSD. Integrn work reqd...
    The <assessmentResult> element (and children) have moved to a new
    namespace. I will have to do some refactoring to accommodate this.

--- qtiworks-jqtiplus/src/main/java/uk/ac/ed/ph/jqtiplus/QtiConstants.java
+++ qtiworks-jqtiplus/src/main/java/uk/ac/ed/ph/jqtiplus/QtiConstants.java
@@ -42,9 +42,11 @@ public final class QtiConstants {
 
     public static final String QTI_20_NAMESPACE_URI = "http://www.imsglobal.org/xsd/imsqti_v2p0";
     public static final String QTI_21_NAMESPACE_URI = "http://www.imsglobal.org/xsd/imsqti_v2p1";
+    public static final String QTI_RESULT_21_NAMESPACE_URI = "http://www.imsglobal.org/xsd/imsqti_result_v2p1";
 
     public static final String QTI_20_SCHEMA_LOCATION = "http://www.imsglobal.org/xsd/imsqti_v2p0.xsd";
     public static final String QTI_21_SCHEMA_LOCATION = "http://www.imsglobal.org/xsd/imsqti_v2p1.xsd";
+    public static final String QTI_RESULT_21_SCHEMA_LOCATION = "http://www.imsglobal.org/xsd/imsqti_result_v2p1.xsd";
 
     public static final String CP_11_NAMESPACE_URI = "http://www.imsglobal.org/xsd/imscp_v1p1";
     public static final String CP_12_NAMESPACE_URI = "http://www.imsglobal.org/xsd/imscp_v1p2";
