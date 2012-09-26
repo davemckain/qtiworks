44b95bbc3d1dc3092db5caa7a30210297c233cd0
David McKain <david.mckain@ed.ac.uk>
2012-09-26 16:59:01 +0100

    Removed Validatable interface

--- qtiworks-jqtiplus/src/main/java/uk/ac/ed/ph/jqtiplus/node/QtiNode.java
+++ qtiworks-jqtiplus/src/main/java/uk/ac/ed/ph/jqtiplus/node/QtiNode.java
@@ -36,8 +36,10 @@ package uk.ac.ed.ph.jqtiplus.node;
 import uk.ac.ed.ph.jqtiplus.attribute.AttributeList;
 import uk.ac.ed.ph.jqtiplus.group.NodeGroupList;
 import uk.ac.ed.ph.jqtiplus.serialization.QtiSaxDocumentFirer;
-import uk.ac.ed.ph.jqtiplus.validation.Validatable;
+import uk.ac.ed.ph.jqtiplus.validation.ValidationContext;
 import uk.ac.ed.ph.jqtiplus.xmlutils.XmlSourceLocationInformation;
+
+import java.io.Serializable;
 
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
@@ -48,7 +50,7 @@ import org.xml.sax.SAXException;
  * @author Jiri Kajaba
  * @author David McKain
  */
-public interface QtiNode extends Validatable, Iterable<QtiNode> {
+public interface QtiNode extends Serializable, Iterable<QtiNode> {
 
     /**
      * Gets parent of this node, or null if this is a {@link RootNode}
@@ -124,6 +126,11 @@ public interface QtiNode extends Validatable, Iterable<QtiNode> {
      * Loads this node from given DOM source {@link Element}.
      */
     void load(Element sourceElement, LoadingContext context);
+
+    /**
+     * Validate this {@link QtiNode} and descends downwards.
+     */
+    void validate(final ValidationContext context);
 
     /** Callback used to serialize this Nodes. Do not call directly. */
     void fireSaxEvents(QtiSaxDocumentFirer qtiSaxDocumentFirer)
