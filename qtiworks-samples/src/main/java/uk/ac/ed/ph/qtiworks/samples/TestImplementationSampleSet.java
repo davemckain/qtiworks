46450c76b3220fe5105ed905e9e074ce7856590a
David McKain <david.mckain@ed.ac.uk>
2012-11-19 12:08:37 +0000

    Added Graham Smith's French Test sample

--- qtiworks-samples/src/main/java/uk/ac/ed/ph/qtiworks/samples/TestImplementationSampleSet.java
+++ qtiworks-samples/src/main/java/uk/ac/ed/ph/qtiworks/samples/TestImplementationSampleSet.java
@@ -43,11 +43,26 @@ import static uk.ac.ed.ph.qtiworks.samples.DeliveryStyle.TEST_WORK_IN_PROGRESS;
  */
 public final class TestImplementationSampleSet {
     
-    private static final QtiSampleSet instance = new QtiSampleSet("Test Implementation Test Samples",
+    private static final QtiSampleSet instance = new QtiSampleSet(
+            "Test Implementation Test Samples",
             "Set of sample assessmentTests used to test our implementation",
+            /* Southampton's "Web Developer Test" (taken and slightly modified from QTIEngine) */
             new QtiSampleAssessment(ASSESSMENT_TEST, TEST_WORK_IN_PROGRESS, "testimplementation/WebDeveloperTest1/template_test1.xml",
                     new String[] { "question1.xml", "question2.xml", "question3.xml", "question4.xml", "question5.xml", "question6.xml", "question7.xml", "question8.xml", "question9.xml" },
-                    new String[] { "directory.jpg" })
+                    new String[] { "directory.jpg" }
+            ),
+            /* Graham Smith's French Test */
+            new QtiSampleAssessment(ASSESSMENT_TEST, TEST_WORK_IN_PROGRESS, "testimplementation/GrahamSmith-French-01/ghfrenchtest01a.xml",
+                    new String[] {
+                        "ghfrench-01-01.xml", "ghfrench-01-erpt.xml", "ghfrench-01-irpt.xml", "ghfrench-01-rept.xml",
+                        "ghfrench-01-vapt1.xml", "ghfrench-01-vapt2.xml", "ghfrench-01-vaux.xml", "ghfrench-01-vaux1rem.xml",
+                        "ghfrench-01-vaux2rem.xml", "ghfrench-01-vauxp1.xml", "ghfrench-01-vauxp2.xml", "ghfrench-01-vauxpt1.xml",
+                        "ghfrench-01-vauxpt2.xml", "ghfrench-01-verp1.xml", "ghfrench-01-verp2.xml", "ghfrench-01-verprem.xml",
+                        "ghfrench-01-virp1.xml", "ghfrench-01-virp2.xml", "ghfrench-01-virprem.xml", "ghfrench-01-vrep1.xml",
+                        "ghfrench-01-vrep2.xml", "ghfrench-01-vreprem.xml"
+                    },
+                    new String[0]
+            )
     );
     
     private TestImplementationSampleSet() {
