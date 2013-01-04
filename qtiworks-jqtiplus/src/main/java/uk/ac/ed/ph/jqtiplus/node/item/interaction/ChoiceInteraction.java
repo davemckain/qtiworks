7dd6f54282b8961f695954f73c6fa49b1aa3fa00
David McKain <david.mckain@ed.ac.uk>
2013-01-04 13:29:51 +0000

    Fix to default value for maxChoices in 5 i'actions
    I discovered that the default value for maxChoices changed from 1 to 0
    somewhere between drafts 2 and 13 of the QTI info model for
    choiceInteraction, hotspotInteraction, hottextInteraction,
    positionObjectInteraction and selectPointInteraction.
    
    These were very poorly described in the changes document. Chances are
    there will be similar issues elsewhere

--- qtiworks-jqtiplus/src/main/java/uk/ac/ed/ph/jqtiplus/node/item/interaction/ChoiceInteraction.java
+++ qtiworks-jqtiplus/src/main/java/uk/ac/ed/ph/jqtiplus/node/item/interaction/ChoiceInteraction.java
@@ -91,7 +91,7 @@ public final class ChoiceInteraction extends BlockInteraction implements SimpleC
     public static final String ATTR_MAX_CHOICES_NAME = "maxChoices";
 
     /** Default value of maxChoices attribute . */
-    public static final int ATTR_MAX_CHOICES_DEFAULT_VALUE = 1;
+    public static final int ATTR_MAX_CHOICES_DEFAULT_VALUE = 0;
 
     /** Name of minChoices attribute in xml schema. */
     public static final String ATTR_MIN_CHOICES_NAME = "minChoices";
