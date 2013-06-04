4d50cb4eef0dbdf70b56af5ef805ce83fbdfb799
David McKain <david.mckain@ed.ac.uk>
2013-06-04 09:30:15 +0100

    Updated M4->M5 schema migration after DEV29 rel.
    This now incorporates all of the changes from DEV26->DEV29.

--- qtiworks-engine/support/schema-migrations/m4-to-m5.sql
+++ qtiworks-engine/support/schema-migrations/m4-to-m5.sql
@@ -1,10 +1,11 @@
 -- Schema migration script for upgrading from 1.0-M4 to 1.0-M5.
+-- (This is not complete yet. It incorporates a merger of DEV26->DEV29 at present)
 --
 -- NB: This has been written to work with PostgreSQL and will probably need
 -- tweaked slightly to work with other databases.
 BEGIN WORK;
 
--- Add support for individual candidate comments
+-- Add support for individual candidate comments in item_delivery_settings
 ALTER TABLE item_delivery_settings ADD allow_candidate_comment boolean;
 UPDATE item_delivery_settings SET allow_candidate_comment = FALSE;
 ALTER TABLE item_delivery_settings ALTER allow_candidate_comment SET NOT NULL;
@@ -14,7 +15,8 @@ ALTER TABLE item_delivery_settings ADD prompt text;
 UPDATE item_delivery_settings ids SET prompt = ds.prompt FROM delivery_settings ds WHERE ds.dsid = ids.dsid;
 ALTER TABLE delivery_settings DROP prompt;
 
--- Ended up removing 'allow result' & 'allow source' in DEV29.
+-- Ended up removing 'allow result' & 'allow source' completely in DEV29.
+-- (This functionality is now merged into author_mode)
 ALTER TABLE item_delivery_settings DROP allow_source;
 ALTER TABLE item_delivery_settings DROP allow_result;
 
@@ -55,11 +57,38 @@ UPDATE item_delivery_settings SET allow_solution_when_ended = allow_solution_whe
 ALTER TABLE item_delivery_settings ALTER allow_solution_when_ended SET NOT NULL;
 ALTER TABLE item_delivery_settings DROP allow_solution_when_closed;
 
+-- Turn author_mode back on for all public delivery settings
+-- (DEV29 also did this for individual users' defaults, but this is probably
+-- risky so I won't do it here.)
+UPDATE delivery_settings SET author_mode = true WHERE public IS TRUE;
+
+-- Change to Assessment -> AssessmentPackage relationships.
+-- First change is setting selected package to be the newest one for each row in assessments
+ALTER TABLE assessments ADD selected_apid bigint REFERENCES assessment_packages(apid);
+UPDATE assessments a SET selected_apid = (SELECT MAX(apid) FROM assessment_packages WHERE aid=a.aid);
+ALTER TABLE assessments ALTER selected_apid SET NOT NULL;
+ALTER TABLE assessment_packages ALTER aid DROP NOT NULL;
+
+-- New columns in assessment_package. We'll set these to defaults for now;
+-- an Engine Manager action should be run to fill in the data appropriately afterwards
+ALTER TABLE assessment_packages ADD launchable boolean;
+ALTER TABLE assessment_packages ADD error_count int;
+ALTER TABLE assessment_packages ADD warning_count int;
+UPDATE assessment_packages SET launchable=valid, error_count=0, warning_count=0;
+ALTER TABLE assessment_packages ALTER launchable SET NOT NULL;
+ALTER TABLE assessment_packages ALTER error_count SET NOT NULL;
+ALTER TABLE assessment_packages ALTER warning_count SET NOT NULL;
+
 -- Some changes to event names
 UPDATE candidate_events SET test_event_type='EXIT_TEST' WHERE test_event_type='EXIT_MULTI_PART_TEST';
 UPDATE candidate_events SET item_event_type='EXIT' WHERE item_event_type='TERMINATE';
 UPDATE candidate_events SET item_event_type='END' WHERE item_event_type='CLOSE';
 UPDATE candidate_events SET item_event_type='ENTER' WHERE item_event_type='INIT';
+
+-- Addition of 'exploded' column to candidate_sessions
+ALTER TABLE candidate_sessions ADD exploded boolean;
+UPDATE candidate_sessions SET exploded=false;
+ALTER TABLE candidate_sessions ALTER exploded SET NOT NULL;
 
 -- Add columns to candidate_session_outcomes
 ALTER TABLE candidate_session_outcomes ADD base_type VARCHAR(14);
