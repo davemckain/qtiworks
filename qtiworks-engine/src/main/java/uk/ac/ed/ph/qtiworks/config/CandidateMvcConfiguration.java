a9cabff9d1fb68ad96ced74e0b75eaf29ecb953e
David McKain <david.mckain@ed.ac.uk>
2013-08-15 17:28:44 +0100

    Ack - reinstanted JSON stuff for candidate MVC
    I forgot that this was required for the mathEntryInteraction
    feedback. So restored now, but only for the candidate MVC

--- qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/config/CandidateMvcConfiguration.java
+++ qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/config/CandidateMvcConfiguration.java
@@ -33,6 +33,8 @@
  */
 package uk.ac.ed.ph.qtiworks.config;
 
+import uk.ac.ed.ph.qtiworks.web.controller.candidate.CandidateHelperController;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -43,6 +45,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -60,12 +63,7 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 @ComponentScan(basePackages={"uk.ac.ed.ph.qtiworks.web.controller.candidate"})
 public class CandidateMvcConfiguration extends WebMvcConfigurerAdapter {
 
-    /**
-     * (I'm setting up message converters explicitly. One reason is that
-     * @ResponseBody doesn't allow you to set an explicit content type,
-     * which can lead to problems. I suppose it's nice and tidy being explicit, so
-     * here we are!)
-     */
+    /** (We need JSON message conversion for the {@link CandidateHelperController}) */
     @Override
     public void configureMessageConverters(final List<HttpMessageConverter<?>> converters) {
         final StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
@@ -74,6 +72,7 @@ public class CandidateMvcConfiguration extends WebMvcConfigurerAdapter {
                 new MediaType("text", "plain", Charsets.UTF_8),
         }));
         converters.add(stringConverter);
+        converters.add(new MappingJackson2HttpMessageConverter());
     }
 
     @Bean
