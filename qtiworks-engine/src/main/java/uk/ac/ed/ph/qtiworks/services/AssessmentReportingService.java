davemckain/qtiworks

commit 5e297fbf497cd84156e57ccd8333d098ba772b15

Fixed bugs in computing model for assessment rept
There were some silly bugs in the logic building the MVC model for
the proctoring functions. These should be fixed here.

qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/services/AssessmentReportingService.java
@@ -61,6 +61,8 @@ import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -117,15 +119,22 @@ public class AssessmentReportingService {
         return buildCandidateSessionSummaryReport(candidateSession);
     }
 
+    /**
+     * Builds a {@link CandidateSessionSummaryReport} for the given {@link CandidateSession}
+     *
+     * @param candidateSession {@link CandidateSession} to report on, which must not be null
+     */
     public CandidateSessionSummaryReport buildCandidateSessionSummaryReport(final CandidateSession candidateSession) {
         Assert.notNull(candidateSession, "candidateSession");
 
+        /* Look up stored outcomes for this session */
         final List<CandidateSessionOutcome> candidateSessionOutcomes = candidateSessionOutcomeDao.getForSession(candidateSession);
+
         /* Convert outcomes into an easy form for manipulating */
-        final LinkedHashSet<String> numericOutcomeIdentifiers = new LinkedHashSet<String>();
-        final LinkedHashSet<String> otherOutcomeIdentifiers = new LinkedHashSet<String>();
-        final LinkedHashSet<String> numericOutcomeValues = new LinkedHashSet<String>();
-        final LinkedHashSet<String> otherOutcomeValues = new LinkedHashSet<String>();
+        final List<String> numericOutcomeIdentifiers = new ArrayList<String>();
+        final List<String> otherOutcomeIdentifiers = new ArrayList<String>();
+        final List<String> numericOutcomeValues = new ArrayList<String>();
+        final List<String> otherOutcomeValues = new ArrayList<String>();
         for (final CandidateSessionOutcome candidateSessionOutcome : candidateSessionOutcomes) {
             final String outcomeIdentifier = candidateSessionOutcome.getOutcomeIdentifier();
             final String outcomeValue = candidateSessionOutcome.getStringValue();
@@ -206,59 +215,67 @@ public class AssessmentReportingService {
 
     public DeliveryCandidateSummaryReport buildDeliveryCandidateSummaryReport(final Delivery delivery) {
         Assert.notNull(delivery, "delivery");
-        final List<CandidateSession> candidateSessions = candidateSessionDao.getForDelivery(delivery);
+
+        /* Look up all outcomes for all sessions associated with this delivery */
         final List<CandidateSessionOutcome> candidateSessionOutcomes = candidateSessionOutcomeDao.getForDelivery(delivery);
 
-        /* Convert outcomes into an easy form for manipulating */
+        /* Group results by each individual CandidateSession, also building up lists (ordered sets) of unique outcome identifiers */
+        final TreeMap<Long, CandidateSession> candidateSessionByIdMap = new TreeMap<Long, CandidateSession>();
         final Map<Long, Map<String, String>> numericOutcomesBySessionIdMap = new HashMap<Long, Map<String,String>>();
         final Map<Long, Map<String, String>> otherOutcomesBySessionIdMap = new HashMap<Long, Map<String,String>>();
-        final LinkedHashSet<String> numericOutcomeIdentifiers = new LinkedHashSet<String>();
-        final LinkedHashSet<String> otherOutcomeIdentifiers = new LinkedHashSet<String>();
+        final LinkedHashSet<String> numericOutcomeIdentifiers = new LinkedHashSet<String>(); /* (Ordered avoiding duplicates) */
+        final LinkedHashSet<String> otherOutcomeIdentifiers = new LinkedHashSet<String>(); /* (Ordered avoiding duplicates) */
         for (final CandidateSessionOutcome candidateSessionOutcome : candidateSessionOutcomes) {
+            /* Do grouping first */
+            final Map<String, String> numericOutcomesForSession, otherOutcomesForSession;
             final CandidateSession candidateSession = candidateSessionOutcome.getCandidateSession();
+            final Long xid = candidateSession.getId();
+            if (!candidateSessionByIdMap.containsKey(xid)) {
+                /* First outcome for this session, so set up appropriate map values */
+                candidateSessionByIdMap.put(xid, candidateSession);
+                numericOutcomesForSession = new HashMap<String, String>();
+                otherOutcomesForSession = new HashMap<String, String>();
+                numericOutcomesBySessionIdMap.put(xid, numericOutcomesForSession);
+                otherOutcomesBySessionIdMap.put(xid, otherOutcomesForSession);
+            }
+            else {
+                numericOutcomesForSession = numericOutcomesBySessionIdMap.get(xid);
+                otherOutcomesForSession = otherOutcomesBySessionIdMap.get(xid);
+            }
+
+            /* Record outcomes */
             final String outcomeIdentifier = candidateSessionOutcome.getOutcomeIdentifier();
             final String outcomeValue = candidateSessionOutcome.getStringValue();
             final BaseType baseType = candidateSessionOutcome.getBaseType();
             if (baseType!=null && baseType.isNumeric() && candidateSessionOutcome.getCardinality()==Cardinality.SINGLE) {
-                numericOutcomeIdentifiers.add(candidateSessionOutcome.getOutcomeIdentifier());
-                Map<String, String> numericOutcomesForSession = numericOutcomesBySessionIdMap.get(candidateSession.getId());
-                if (numericOutcomesForSession==null) {
-                    numericOutcomesForSession = new HashMap<String, String>();
-                    numericOutcomesBySessionIdMap.put(candidateSession.getId(), numericOutcomesForSession);
-                }
+                numericOutcomeIdentifiers.add(outcomeIdentifier);
                 numericOutcomesForSession.put(outcomeIdentifier, outcomeValue);
             }
             else {
-                otherOutcomeIdentifiers.add(candidateSessionOutcome.getOutcomeIdentifier());
-                Map<String, String> otherOutcomesForSession = otherOutcomesBySessionIdMap.get(candidateSession.getId());
-                if (otherOutcomesForSession==null) {
-                    otherOutcomesForSession = new HashMap<String, String>();
-                    otherOutcomesBySessionIdMap.put(candidateSession.getId(), otherOutcomesForSession);
-                }
+                otherOutcomeIdentifiers.add(outcomeIdentifier);
                 otherOutcomesForSession.put(outcomeIdentifier, outcomeValue);
             }
         }
 
+        /* Record metadata */
         final String ltiResultOutcomeIdentifier = delivery.getAssessment().getLtiResultOutcomeIdentifier();
         final CandidateSessionSummaryMetadata summaryMetadata = new CandidateSessionSummaryMetadata(ltiResultOutcomeIdentifier, numericOutcomeIdentifiers, otherOutcomeIdentifiers);
 
         /* Now build report for each session */
         final List<CandidateSessionSummaryData> rows = new ArrayList<CandidateSessionSummaryData>();
-        for (int i=0; i<candidateSessions.size(); i++) {
-            final CandidateSession candidateSession = candidateSessions.get(i);
+        for (final Entry<Long, CandidateSession> entry : candidateSessionByIdMap.entrySet()) {
+            final long xid = entry.getKey();
+            final CandidateSession candidateSession = entry.getValue();
             final List<String> numericOutcomeValues = new ArrayList<String>();
-            final Map<String, String> numericOutcomesForSession = numericOutcomesBySessionIdMap.get(candidateSession.getId());
-            if (numericOutcomesForSession!=null) {
-                for (final String outcomeIdentifier : numericOutcomeIdentifiers) {
-                    numericOutcomeValues.add(StringUtilities.emptyIfNull(numericOutcomesForSession.get(outcomeIdentifier)));
-                }
-            }
             final List<String> otherOutcomeValues = new ArrayList<String>();
-            final Map<String, String> otherOutcomesForSession = otherOutcomesBySessionIdMap.get(candidateSession.getId());
-            if (otherOutcomesForSession!=null) {
-                for (final String outcomeIdentifier : otherOutcomeIdentifiers) {
-                    otherOutcomeValues.add(StringUtilities.emptyIfNull(otherOutcomesForSession.get(outcomeIdentifier)));
-                }
+
+            final Map<String, String> numericOutcomesForSession = numericOutcomesBySessionIdMap.get(xid);
+            for (final String outcomeIdentifier : numericOutcomeIdentifiers) {
+                numericOutcomeValues.add(numericOutcomesForSession.get(outcomeIdentifier));
+            }
+            final Map<String, String> otherOutcomesForSession = otherOutcomesBySessionIdMap.get(xid);
+            for (final String outcomeIdentifier : otherOutcomeIdentifiers) {
+                otherOutcomeValues.add(otherOutcomesForSession.get(outcomeIdentifier));
             }
             String ltiResultOutcomeValue = null;
             if (ltiResultOutcomeIdentifier!=null && numericOutcomesForSession!=null) {

qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/domain/entities/CandidateSessionOutcome.java
@@ -72,7 +72,7 @@ import org.hibernate.annotations.Type;
             query="SELECT xo"
                 + "  FROM CandidateSessionOutcome xo"
                 + "  WHERE xo.candidateSession.delivery = :delivery"
-                + "  ORDER BY xo.id"),
+                + "  ORDER BY xo.candidateSession.xid, xo.id"),
     @NamedQuery(name="CandidateSessionOutcome.deleteForSession",
             query="DELETE FROM CandidateSessionOutcome xo"
                 + "  WHERE xo.candidateSession = :candidateSession")

qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/services/domain/CandidateSessionSummaryData.java
@@ -40,7 +40,6 @@ import uk.ac.ed.ph.jqtiplus.internal.util.Assert;
 import uk.ac.ed.ph.jqtiplus.internal.util.ObjectUtilities;
 
 import java.io.Serializable;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -84,7 +83,7 @@ public final class CandidateSessionSummaryData implements Serializable {
             final boolean sessionClosed, final boolean sessionTerminated, final boolean sessionExploded,
             final LisOutcomeReportingStatus lisOutcomeReportingStatus,
             final String lisResultOutcomeValue, final Double lisScore,
-            final Collection<String> numericOutcomeValues, final Collection<String> otherOutcomeValues) {
+            final List<String> numericOutcomeValues, final List<String> otherOutcomeValues) {
         Assert.notNull(numericOutcomeValues, "numericOutcomeValues");
         Assert.notNull(otherOutcomeValues, "otherOutcomesValues");
         this.sessionId = sessionId;

qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/services/domain/CandidateSessionSummaryMetadata.java
@@ -36,7 +36,6 @@ package uk.ac.ed.ph.qtiworks.services.domain;
 import uk.ac.ed.ph.jqtiplus.internal.util.ObjectUtilities;
 
 import java.io.Serializable;
-import java.util.Collection;
 import java.util.List;
 
 import com.google.common.collect.ImmutableList;
@@ -60,8 +59,8 @@ public final class CandidateSessionSummaryMetadata implements Serializable {
     private final ImmutableList<String> otherOutcomeIdentifiers;
 
     public CandidateSessionSummaryMetadata(final String lisResultOutcomeIdentifier,
-            final Collection<String> numericOutcomeIdentifiers,
-            final Collection<String> otherOutcomeIdentifiers) {
+            final Iterable<String> numericOutcomeIdentifiers,
+            final Iterable<String> otherOutcomeIdentifiers) {
         this.lisResultOutcomeIdentifier = lisResultOutcomeIdentifier;
         this.numericOutcomeIdentifiers = ImmutableList.<String>copyOf(numericOutcomeIdentifiers);
         this.otherOutcomeIdentifiers = ImmutableList.<String>copyOf(otherOutcomeIdentifiers);

commit 9dba4199641e05d2151b6097d3a32f4450f13680

Fixed regression introduced in 5e297fb
The new logic failed to correctly account for the possibility
of there being different variables in different sessions, which
can occur if the assessment package is replaced at some point.

Missing variables now generate a 'N/A' result in the delivery
reports.

qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/services/AssessmentReportingService.java
@@ -271,15 +271,15 @@ public class AssessmentReportingService {
 
             final Map<String, String> numericOutcomesForSession = numericOutcomesBySessionIdMap.get(xid);
             for (final String outcomeIdentifier : numericOutcomeIdentifiers) {
-                numericOutcomeValues.add(numericOutcomesForSession.get(outcomeIdentifier));
+                numericOutcomeValues.add(safelyExtractOutcomeValue(numericOutcomesForSession, outcomeIdentifier));
             }
             final Map<String, String> otherOutcomesForSession = otherOutcomesBySessionIdMap.get(xid);
             for (final String outcomeIdentifier : otherOutcomeIdentifiers) {
-                otherOutcomeValues.add(otherOutcomesForSession.get(outcomeIdentifier));
+                otherOutcomeValues.add(safelyExtractOutcomeValue(otherOutcomesForSession, outcomeIdentifier));
             }
             String ltiResultOutcomeValue = null;
             if (ltiResultOutcomeIdentifier!=null && numericOutcomesForSession!=null) {
-                ltiResultOutcomeValue = numericOutcomesForSession.get(ltiResultOutcomeIdentifier);
+                ltiResultOutcomeValue = safelyExtractOutcomeValue(numericOutcomesForSession, ltiResultOutcomeIdentifier);
             }
             final User candidate = candidateSession.getCandidate();
             final CandidateSessionSummaryData row = new CandidateSessionSummaryData(candidateSession.getId().longValue(),
@@ -300,6 +300,15 @@ public class AssessmentReportingService {
 
         auditLogger.recordEvent("Generated candidate summary report for Delivery #" + delivery.getId());
         return new DeliveryCandidateSummaryReport(summaryMetadata, rows);
+    }
+
+    /**
+     * Safely extracts an outcome value from the given Map, handling the case where the Map is null
+     * or the value does not exist sensible. (These scenarios may happen if the assessment package changes
+     * over time.)
+     */
+    private String safelyExtractOutcomeValue(final Map<String, String> outcomesByIdentifier, final String identifier) {
+        return outcomesByIdentifier!=null && outcomesByIdentifier.containsKey(identifier) ? outcomesByIdentifier.get(identifier) : "N/A";
     }
 
     //-------------------------------------------------