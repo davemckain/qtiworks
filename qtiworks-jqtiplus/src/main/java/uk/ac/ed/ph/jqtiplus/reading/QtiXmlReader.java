79b1a6ca29305eec59e7d99db3ca2f249cafe0a6
Stanley, James T <jstanley@unicon.net>
2016-02-08 12:34:13 -0700

    NOJIRA added MathML schema to support validation against MathMl

--- qtiworks-jqtiplus/src/main/java/uk/ac/ed/ph/jqtiplus/reading/QtiXmlReader.java
+++ qtiworks-jqtiplus/src/main/java/uk/ac/ed/ph/jqtiplus/reading/QtiXmlReader.java
@@ -110,7 +110,7 @@ public final class QtiXmlReader {
         resultingSchemaMapTemplate.put(QtiConstants.QTI_21_NAMESPACE_URI, QtiConstants.QTI_21_SCHEMA_LOCATION);
         resultingSchemaMapTemplate.put(QtiConstants.QTI_RESULT_21_NAMESPACE_URI, QtiConstants.QTI_RESULT_21_SCHEMA_LOCATION);
         resultingSchemaMapTemplate.put(QtiConstants.QTI_20_NAMESPACE_URI, QtiConstants.QTI_20_SCHEMA_LOCATION);
-
+        resultingSchemaMapTemplate.put(QtiConstants.MATHML_NAMESPACE_URI, QtiConstants.MATHML_SCHEMA_LOCATION);
         this.jqtiExtensionManager = jqtiExtensionManager;
         this.xmlResourceReader = new XmlResourceReader(JQTIPLUS_PARSER_RESOURCE_LOCATOR, resultingSchemaMapTemplate, schemaCache);
     }
