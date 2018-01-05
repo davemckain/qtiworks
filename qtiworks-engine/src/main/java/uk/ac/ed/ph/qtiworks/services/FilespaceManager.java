davemckain/qtiworks

commit 0685ea4409a98eefc25a61f8be10482a349d745b

Minor code simplification

qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/services/FilespaceManager.java
@@ -75,21 +75,21 @@ public final class FilespaceManager {
     @Resource
     private RequestTimestampContext requestTimestampContext;
 
-    private File filesystemBaseDirectory;
+    private String filesystemBaseUri;
 
     @PostConstruct
     public void init() {
         final String filesystemBaseString = qtiWorksDeploymentSettings.getFilesystemBase();
-        logger.info("Filesystem base for client data is {}", filesystemBaseString);
-        this.filesystemBaseDirectory = new File(filesystemBaseString);
+        final File filesystemBaseDirectory = new File(filesystemBaseString);
         if (!filesystemBaseDirectory.isDirectory()) {
             throw new QtiWorksRuntimeException("Filesystem base path " + filesystemBaseString + " is not a directory");
         }
+        this.filesystemBaseUri = filesystemBaseDirectory.toURI().toString().replaceFirst("/$", "");
+        logger.info("Filesystem base for client data is {}", filesystemBaseString);
     }
 
     public File createTempFile() {
-        final String tmpFolderUri = filesystemBaseDirectory.toURI().toString()
-                + "/tmp";
+        final String tmpFolderUri = filesystemBaseUri + "/tmp";
         final File tmpFolder = ensureCreateDirectory(tmpFolderUri);
         return new File(tmpFolder, createUniqueRequestComponent());
     }
@@ -127,8 +127,7 @@ public final class FilespaceManager {
     }
 
     private String getAssessmentPackageSandboxBaseUri() {
-        return filesystemBaseDirectory.toURI().toString()
-                + "/assessments";
+        return filesystemBaseUri + "assessments";
     }
 
     //-------------------------------------------------
@@ -155,8 +154,7 @@ public final class FilespaceManager {
     }
 
     private String getCandidateUploadBaseUri() {
-        return filesystemBaseDirectory.toURI().toString()
-                + "/responses";
+        return filesystemBaseUri + "/responses";
     }
 
     private String getCandidateSessionUploadBaseUri(final Delivery delivery) {
@@ -200,8 +198,7 @@ public final class FilespaceManager {
     }
 
     private final String getCandidateSessionStoreBaseUri() {
-        return filesystemBaseDirectory.toURI().toString()
-                + "/sessions";
+        return filesystemBaseUri + "/sessions";
     }
 
     private final String getCandidateSessionStoreBaseUri(final Delivery delivery) {

commit d085058a7bde56482989e1ee1f77189059215332

Fixed #62: Added empty file store directory purge
I've added some functionality to purge empty directories from the
QTIWorks file store. This is performed along with the other routine
maintenance tasks, and can also be invoked from the engine manager.

[This commit also fixes a minor regression introduced in 0685ea4.]

qtiworks-engine-manager/src/main/resources/logback.xml
@@ -17,6 +17,7 @@ if you prefer. See the documentation.
 
   <logger name="uk.ac.ed.ph.qtiworks.manager" level="info"/>
   <logger name="uk.ac.ed.ph.qtiworks.services.DataDeletionService" level="info"/>
+  <logger name="uk.ac.ed.ph.qtiworks.services.MaintenanceJobService" level="info"/>
 
   <!-- Silence warnings from Hibernate about narrowing proxies -->
   <logger name="org.hibernate.engine.internal.StatefulPersistenceContext" level="error"/>

qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/services/FilespaceManager.java
@@ -127,7 +127,7 @@ public final class FilespaceManager {
     }
 
     private String getAssessmentPackageSandboxBaseUri() {
-        return filesystemBaseUri + "assessments";
+        return filesystemBaseUri + "/assessments";
     }
 
     //-------------------------------------------------
@@ -237,6 +237,44 @@ public final class FilespaceManager {
     public void deleteSandbox(final File sandboxDirectory) {
         Assert.notNull(sandboxDirectory, "sandboxDirectory");
         recursivelyDeleteDirectory(sandboxDirectory);
+    }
+
+    //-------------------------------------------------
+
+    /**
+     * Prunes empty subdirectories within the QTIWorks file store.
+     *
+     * @return the total number of empty subdirectories deleted
+     */
+    public int purgeEmptyStoreDirectories() {
+        int deletedCount;
+        deletedCount = purgeStoreDirectoryIfEmpty(fileUriToFile(getCandidateSessionStoreBaseUri()));
+        deletedCount += purgeStoreDirectoryIfEmpty(fileUriToFile(getCandidateUploadBaseUri()));
+        deletedCount += purgeStoreDirectoryIfEmpty(fileUriToFile(getAssessmentPackageSandboxBaseUri()));
+        return deletedCount;
+    }
+
+    private int purgeStoreDirectoryIfEmpty(final File directory) {
+        /* Perform depth first search */
+        int deletedCount = 0;
+        final File[] childFiles = directory.listFiles();
+        for (final File childFile : childFiles) {
+            if (childFile.isDirectory()) {
+                deletedCount += purgeStoreDirectoryIfEmpty(childFile);
+            }
+        }
+
+        /* Then delete this directory if it is (now) empty */
+        if (directory.listFiles().length == 0) {
+            logger.debug("Deleting empty store directory {}", directory);
+            if (directory.delete()) {
+                ++deletedCount;
+            }
+            else {
+                logger.warn("Unexpected failure to delete apparently empty store directory {}", directory);
+            }
+        }
+        return deletedCount;
     }
 
     //-------------------------------------------------

qtiworks-engine/src/main/java/uk/ac/ed/ph/qtiworks/services/MaintenanceJobService.java
@@ -62,6 +62,9 @@ public class MaintenanceJobService {
     private DataDeletionService dataDeletionService;
 
     @Resource
+    private FilespaceManager filespaceManager;
+
+    @Resource
     private LtiOutcomeService ltiOutcomeService;
 
     //-------------------------------------------------
@@ -74,6 +77,7 @@ public class MaintenanceJobService {
         purgeTransientData(beforeTimestamp);
         purgeOldNonces(beforeTimestamp);
         dataDeletionService.purgeOrphanedLtiCandidateUsers();
+        purgeEmptyStoreDirectories();
 
         final long afterTimestamp = System.currentTimeMillis();
         final long duration = afterTimestamp - beforeTimestamp;
@@ -90,11 +94,21 @@ public class MaintenanceJobService {
     }
 
     /**
-     * Pureges OAuth nonces for LTI launches that were created more than
+     * Purges OAuth nonces for LTI launches that were created more than
      * {@link DomainConstants#OAUTH_TIMESTAMP_MAX_AGE} milliseconds ago.
      */
     private void purgeOldNonces(final long currentTimestamp) {
         final Date nonceThreshold = new Date(currentTimestamp - DomainConstants.OAUTH_TIMESTAMP_MAX_AGE);
         dataDeletionService.purgeOldNonces(nonceThreshold);
+    }
+
+    /**
+     * Purges empty directories in the QTIWorks filestore
+     */
+    private void purgeEmptyStoreDirectories() {
+        final int deletedCount = filespaceManager.purgeEmptyStoreDirectories();
+        if (deletedCount > 0) {
+            logger.info("Purged {} empty filestore directories", deletedCount);
+        }
     }
 }

qtiworks-engine/src/main/resources/logback.xml
@@ -35,8 +35,9 @@ if you prefer. See the documentation.
 
   <!-- Also log certain other services at info level -->
   <logger name="uk.ac.ed.ph.qtiworks.config.ServicesConfiguration" level="info"/>
+  <logger name="uk.ac.ed.ph.qtiworks.services.ScheduledService" level="info"/>
+  <logger name="uk.ac.ed.ph.qtiworks.services.MaintenanceJobService" level="info"/>
   <logger name="uk.ac.ed.ph.qtiworks.web.QtiworksWebApplicationContextInitializer" level="info"/>
-  <logger name="uk.ac.ed.ph.qtiworks.services.ScheduledServices" level="info"/>
 
   <!-- Silence warnings from Hibernate about narrowing proxies -->
   <logger name="org.hibernate.engine.internal.StatefulPersistenceContext" level="error"/>